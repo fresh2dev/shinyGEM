6519c92ee5c02550cd998bef6dc1bd8df5a159a0
Donald Mellenbruch <dmellenbruch>
2019-05-01 17:16:05 -0500

    install only missing dependencies

--- install-requirements.R
+++ install-requirements.R
@@ -1,19 +1,29 @@
-install.packages(c('shiny',
-                'shinydashboard',
-                'shinycssloaders',
-                'DT',
-                'rlang',
-                'data.table',
-                'readxl',
-                'readr',
-                'magrittr',
-                'scales',
-                'ggplot2',
-                'ggthemes',
-                'ggrepel',
-                'gridExtra',
-                'GGally',
-                'lubridate',
-                'anytime',
-                'devtools',
-                'ggiraph'))
+# https://stackoverflow.com/a/4090208
+
+list.of.packages <- c('shiny',
+                    'shinydashboard',
+                    'shinycssloaders',
+                    'DT',
+                    'rlang',
+                    'data.table',
+                    'readxl',
+                    'readr',
+                    'magrittr',
+                    'scales',
+                    'ggplot2',
+                    'ggthemes',
+                    'ggrepel',
+                    'gridExtra',
+                    'GGally',
+                    'lubridate',
+                    'anytime',
+                    'devtools',
+                    'ggiraph')
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages) > 0) {
+    install.packages(new.packages)
+}
+
+
